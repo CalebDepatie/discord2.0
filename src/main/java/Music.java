1c3d64cc87259fa1aab3f42ecca740ace911c9fa
Alexa Walker <55329868+AlexaWalker@users.noreply.github.com>
2020-03-24 19:26:13 -0400

    some music things

---
+++ src/main/java/Music.java
@@ -0,0 +1,36 @@
+package sample;
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+
+public class Music{
+
+    public static enum Volume {
+        MUTE, LOW, MEDIUM, HIGH
+    }
+    public static Volume volume = Volume.LOW;
+
+    public void SoundClipTest() {
+        try {
+            File soundFile = new File("Music.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
